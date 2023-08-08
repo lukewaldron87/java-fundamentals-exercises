160a99546bf359577abda427b747a1e3555d3706
Luke Waldron <lukewaldron87@gmail.com>
2023-08-08 16:10:06 +0200

    finished HelloAnnotationsExercise

---
+++ 1-0-java-basics/1-5-0-hello-annotations/src/main/java/com/bobocode/basics/Exercise.java
@@ -0,0 +1,16 @@
+package com.bobocode.basics;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Exercise{
+
+    public String value() default "";
+    public Level complexityLevel() default Level.BASIC;
+
+
+}
